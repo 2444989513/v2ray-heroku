68af3de5da67463b0cff07d3df02bb85d0828cc5
2444989513 <56295510+2444989513@users.noreply.github.com>
2020-03-23 05:26:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Download and install V2Ray
-curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+# Download and install V2Ray https://github.com/v2ray/v2ray-core/releases/download/v4.23.0/v2ray-linux-64.zip
+#curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.23.0/v2ray-linux-64.zip
 mkdir /usr/bin/v2ray /etc/v2ray
 touch /etc/v2ray/config.json
 unzip /v2ray.zip -d /usr/bin/v2ray
@@ -17,21 +18,53 @@ cat <<-EOF > /etc/v2ray/config.json
       "clients": [
         {
           "id": "${UUID}",
-          "alterId": 64
+          "alterId": 2
         }
       ]
     },
-    "streamSettings": {
-      "network": "ws"
-    }
+      "streamSettings": {
+        "network": "ws", 
+        "wsSettings": {
+          "path": "/etc/html/www/nytbrt.html"
+        }
+      }
   }
   ],
   "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
+    {
+      "protocol": "freedom", 
+      "settings": {
+        "domainStrategy": "UseIP"
+	  }, 
+      "tag": "direct"
+    }, 
+    {
+      "protocol": "blackhole", 
+      "settings": { }, 
+      "tag": "blocked"
+    }
+  ], 
+    "dns": {
+    "servers": [
+	"https+local://1.1.1.1/dns-query",
+	"https+local://1.0.0.1/dns-query",
+	"https+local://dns.google/dns-query",
+	"https+local://dns.google/resolve",
+	"localhost"
+    ]
+  },
+  "routing": {
+    "domainStrategy": "IPOnDemand",
+    "rules": [
+      {
+        "type": "field",
+        "inboundTag": [
+          "vmess-in"
+        ],
+        "outboundTag": "direct"
+      }
+    ]
   }
-  ]
 }
 EOF
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
